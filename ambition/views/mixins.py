ed521865845339f6f11abdd0ce14d587ad604010
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-05-07 22:34:36 +0200

    update url value

--- ambition/views/mixins.py
+++ ambition/views/mixins.py
@@ -10,8 +10,7 @@ class AppConfigListboardUrlsViewMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
-            subject_listboard_url_name=django_apps.get_app_config(
-                'ambition').dashboard_url_name,
+            ambition_subject_dashboard_url_name=django_apps.get_app_config('ambition_subject').dashboard_url_name,
             dashboard_url_name=django_apps.get_app_config(
                 self.dashboard_url_app_label).dashboard_url_name,
         )
