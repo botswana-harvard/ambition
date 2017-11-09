8e1df2b6d0b3c1c76b3cf36de61b06afff083585
erikvw <ew2789@gmail.com>
2017-11-09 18:04:28 +0200

    add home view warning for empty sid list

--- ambition/views/home_view.py
+++ ambition/views/home_view.py
@@ -1,9 +1,12 @@
+from django.apps import apps as django_apps
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_dashboard.view_mixins import AppConfigViewMixin
 from edc_navbar import NavbarViewMixin
+from django.contrib import messages
+from ambition_rando.randomizer import Randomizer
 
 
 class HomeView(EdcBaseViewMixin, AppConfigViewMixin, NavbarViewMixin, TemplateView):
@@ -20,4 +23,14 @@ class HomeView(EdcBaseViewMixin, AppConfigViewMixin, NavbarViewMixin, TemplateVi
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        self.verify_sid_list()
         return context
+
+    def verify_sid_list(self):
+        model_cls = django_apps.get_model(Randomizer.sid_model)
+        if model_cls.objects.all().count() == 0:
+            messages.error(
+                self.request,
+                'Randomization list has not been loaded. '
+                'Run the \'import_randomization_list\' management command '
+                'to load before using the system.')
