92500825c1464fa238f36c7cfb8684ddc54b4139
erikvw <ew2789@gmail.com>
2017-11-09 22:11:14 +0200

    update requirements

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import os
 
 SOURCE_ROOT = os.path.expanduser('~/source')
 bind = "127.0.0.1:9000"  # Don't use port 80 because nginx occupied it already.
-errorlog = os.path.join(SOURCE_ROOT, 'logs/gunicorn-error.log')
-accesslog = os.path.join(SOURCE_ROOT, 'logs/gunicorn-access.log')
+errorlog = os.path.join(SOURCE_ROOT, 'logs/ambition-gunicorn-error.log')
+accesslog = os.path.join(SOURCE_ROOT, 'logs/ambition-gunicorn-access.log')
 loglevel = 'debug'
 workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
