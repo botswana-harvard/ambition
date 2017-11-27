1f28d020fbcb8ea2b2826c8997bdffc6e1a26d2d
Leslie <leslie.kep@gmail.com>
2017-11-27 12:29:41 +0200

    add middleware to settings_production

--- ambition/settings_production.py
+++ ambition/settings_production.py
@@ -86,6 +86,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'edc_dashboard.middleware.DashboardMiddleware',
+    'edc_subject_dashboard.middleware.DashboardMiddleware',
+    'edc_lab_dashboard.middleware.DashboardMiddleware',
 ]
 
 ROOT_URLCONF = 'ambition.urls'
