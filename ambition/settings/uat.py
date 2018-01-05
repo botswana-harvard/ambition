a4f0daef582591b46857c729b7574005570dae94
Leslie <leslie.kep@gmail.com>
2018-01-05 09:45:07 +0200

    set site id

--- ambition/settings/uat.py
+++ ambition/settings/uat.py
@@ -5,7 +5,7 @@ DEBUG = False
 ETC_DIR = os.path.join('/etc', APP_NAME)
 
 SECRET_KEY = '2^p0phb&x&ntbsduf6afw(@efi(+!&hm_lrjr-+$5v(t0_f+6t'
-
+SITE_ID = '10'
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ambition-test.bhp.org.bw']
 
 DATABASES = {
