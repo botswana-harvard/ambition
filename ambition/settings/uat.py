191e85a6cff6a564d0bb8c15d1cf05335377d203
erikvw <ew2789@gmail.com>
2018-01-05 16:33:54 +0200

    point uat server to test rando list

--- ambition/settings/uat.py
+++ ambition/settings/uat.py
@@ -5,8 +5,15 @@ DEBUG = False
 ETC_DIR = os.path.join('/etc', APP_NAME)
 
 SECRET_KEY = '2^p0phb&x&ntbsduf6afw(@efi(+!&hm_lrjr-+$5v(t0_f+6t'
+
+# site is gaborone
 SITE_ID = '10'
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ambition-test.bhp.org.bw']
+
+# use the test rando list
+RANDOMIZATION_LIST_PATH = os.path.join(
+    BASE_DIR, APP_NAME, 'tests', 'test_randomization_list.csv')
 
 DATABASES = {
     'default': {
