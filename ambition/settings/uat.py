75ef807bfa410900a312a3f40cdc71a9f298f1ef
erikvw <ew2789@gmail.com>
2018-01-07 13:03:33 +0200

    update logger to use syslog

--- ambition/settings/uat.py
+++ ambition/settings/uat.py
@@ -47,6 +47,14 @@ CACHES = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
     'formatters': {
         'verbose': {
             'format': '%(process)-5d %(thread)d %(name)-50s %(levelname)-8s %(message)s'
