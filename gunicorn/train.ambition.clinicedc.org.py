137792b7890f9b964f2a980913a434d017bbe494
erikvw <ew2789@gmail.com>
2018-01-24 20:45:50 +0200

    gunicorn

--- gunicorn/train.ambition.clinicedc.org.py
+++ gunicorn/train.ambition.clinicedc.org.py
@@ -6,4 +6,8 @@ accesslog = os.path.join(SOURCE_ROOT, 'log/ambition-test-gunicorn-access.log')
 loglevel = 'debug'
 workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
 
+pidfile = os.path.join(SOURCE_ROOT, 'run/ambition-train.pid')
+
+raw_env = [f'DJANGO_SETTINGS_MODULE=ambition.settings.production.train']
+
 bind = "127.0.0.1:9102"
--- gunicorn/uat.ambition.clinicedc.org.py
+++ gunicorn/uat.ambition.clinicedc.org.py
@@ -6,4 +6,8 @@ accesslog = os.path.join(SOURCE_ROOT, 'log/ambition-test-gunicorn-access.log')
 loglevel = 'debug'
 workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
 
+pidfile = os.path.join(SOURCE_ROOT, 'run/ambition-uat.pid')
+
+raw_env = [f'DJANGO_SETTINGS_MODULE=ambition.settings.production.uat']
+
 bind = "127.0.0.1:9101"
