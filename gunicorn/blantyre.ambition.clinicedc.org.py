8662508abed34e4ca91e6a8eaea86ec4add64842
erikvw <ew2789@gmail.com>
2018-01-24 20:31:04 +0200

    gunicorn

--- gunicorn/blantyre.ambition.clinicedc.org.py
+++ gunicorn/blantyre.ambition.clinicedc.org.py
@@ -1,10 +1,15 @@
 # blantyre.ambition gunicorn.conf
 import os
 
-SOURCE_ROOT = os.path.expanduser('~/')
+SOURCE_ROOT = os.path.expanduser('~/source/ambition')
+
 errorlog = os.path.join(SOURCE_ROOT, 'log/ambition-gunicorn-error.log')
 accesslog = os.path.join(SOURCE_ROOT, 'log/ambition-gunicorn-access.log')
 loglevel = 'debug'
+pidfile = os.path.join(SOURCE_ROOT, 'run/ambition-blantyre.pid')
+
 workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
+
+raw_env = [f'DJANGO_SETTINGS_MODULE=ambition.settings.production.blantyre']
 
 bind = "127.0.0.1:9040"
