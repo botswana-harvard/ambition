ec53513e298ac246670ff3bf22b6e090f3644663
erikvw <ew2789@gmail.com>
2018-01-24 14:04:01 +0200

    gunicorn

--- gunicorn/lilongwe.ambition.clinicedc.org.py
+++ gunicorn/lilongwe.ambition.clinicedc.org.py
@@ -1,9 +1,8 @@
 # lilongwe.ambition gunicorn.conf
 import os
 
-SOURCE_ROOT = os.path.expanduser('~/')
-errorlog = os.path.join(SOURCE_ROOT, 'logs/ambition-gunicorn-error.log')
-accesslog = os.path.join(SOURCE_ROOT, 'logs/ambition-gunicorn-access.log')
+errorlog = '/var/log/ambition-gunicorn-error.log'
+accesslog = '/var/log/ambition-gunicorn-access.log'
 loglevel = 'debug'
 workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
 
