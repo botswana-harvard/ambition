d5d58cd01328bc13ccf5d88f1fc06ed09bd092a3
erikvw <ew2789@gmail.com>
2018-01-25 16:43:38 +0200

    minor

--- gunicorn/uat.ambition.clinicedc.org.py
+++ gunicorn/uat.ambition.clinicedc.org.py
@@ -1,3 +1,4 @@
+# uat.ambition gunicorn.conf
 import os
 
 from pathlib import Path
@@ -7,10 +8,10 @@ SOURCE_ROOT = str(Path(os.path.dirname(os.path.abspath(__file__))).parent)
 errorlog = os.path.join(SOURCE_ROOT, 'log/ambition-test-gunicorn-error.log')
 accesslog = os.path.join(SOURCE_ROOT, 'log/ambition-test-gunicorn-access.log')
 loglevel = 'debug'
-workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
-
 pidfile = os.path.join(SOURCE_ROOT, 'run/ambition-uat.pid')
 
-raw_env = [f'DJANGO_SETTINGS_MODULE=ambition.settings.test.uat']
+workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
+
+raw_env = ['DJANGO_SETTINGS_MODULE=ambition.settings.test.uat']
 
 bind = "127.0.0.1:9101"
