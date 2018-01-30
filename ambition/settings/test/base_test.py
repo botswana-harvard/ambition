b41455a54d6bad5dc106153f615cdcbd62f5841b
erikvw <ew2789@gmail.com>
2018-01-30 14:22:02 +0200

    uat settings

--- ambition/settings/test/base_test.py
+++ ambition/settings/test/base_test.py
@@ -5,7 +5,7 @@ from ..logging import LOGGING
 
 DEBUG = False
 
-WARNING_MESSAGE = 'This is a test system. Do not use for production data collection! '
+# WARNING_MESSAGE = 'This is a test system. Do not use for production data collection! '
 
 ETC_DIR = os.path.join('/etc', APP_NAME, 'test')
 
