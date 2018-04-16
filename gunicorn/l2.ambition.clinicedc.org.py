0ef9f9c79602ae2ffe450e30f44a264839ff092b
Leslie <leslie.kep@gmail.com>
2018-04-16 14:04:45 +0200

    l2

---
+++ gunicorn/l2.ambition.clinicedc.org.py
@@ -0,0 +1,19 @@
+# uat.ambition gunicorn.conf
+import os
+
+from pathlib import Path
+
+SOURCE_ROOT = str(Path(os.path.dirname(os.path.abspath(__file__))).parent)
+
+errorlog = os.path.join(
+    SOURCE_ROOT, 'log/ambition-l2-gunicorn-error.log')
+accesslog = os.path.join(
+    SOURCE_ROOT, 'log/ambition-l2-gunicorn-access.log')
+loglevel = 'debug'
+pidfile = os.path.join(SOURCE_ROOT, 'run/ambition-l2.pid')
+
+workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
+
+raw_env = ['DJANGO_SETTINGS_MODULE=ambition.settings.test.l2']
+
+bind = "127.0.0.1:9170"
